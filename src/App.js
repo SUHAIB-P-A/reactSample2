5f46f4e35bc43d1f186874ff5fbfc1becbce7fba
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-09 11:44:13 +0530

    commit 1 counter with normal function next arrow function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
-
 function App() {
+  let count = 0;
+  function addCount() {
+    count = count + 1;
+    console.log(count);
+  }
   return (
-    <h1>hello</h1>
+    <div>
+      <button onClick={addCount}>
+        Add Count
+      </button>
+      <h1>counter: {count}</h1>
+    </div>
   );
 }
 
