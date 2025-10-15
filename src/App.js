4537b4ea4cd98997828986a04fbf1bf6f025c631
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-15 06:03:25 +0530

    commit 8 iterating a list of data using map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Counter from "./components/counter";
+//import Counter from "./components/counter";
+import Employee from "./components/employee";
 import { useState } from "react";
 
 function App() {
@@ -13,15 +14,24 @@ function App() {
   }
 
   // spread operator example              // rest operator example
-  let obj = { title: "1st Counter", count,place: "India" };
+  //let obj = { title: "1st Counter", count,place: "India" };
+  // map helper example variable
+  let emp = [{ name: "suhaib", age: 21 }, { name: "arif", age: 22 }, { name: "sajid", age: 23 }];
   return (
     <div>
       <button onClick={addCount}>
         Add Count
       </button>
       {/* use of spread operator ({...obj}) */}
-      <Counter {...obj} />
-      <Counter title={"2nd Counter"} count={count} />
+      {/*<Counter {...obj} />*/}
+      {/*<Counter title={"2nd Counter"} count={count} />*/}
+
+      {/* use of map helper */}
+      {
+        emp.map((emp) => {
+          return (<Employee name={emp.name} age={emp.age} />);
+        })
+      }
     </div>
   );
 }
---
+++ src/components/employee.jsx
@@ -0,0 +1,11 @@
+
+
+function Employee(props) {
+  return (
+    <div>
+      <h1>Name : {props.name} , Age : {props.age}</h1>
+    </div>
+  )
+}
+
+export default Employee
